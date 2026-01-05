ca33819d4a64568568eca13e19903601c935b27c
oriatta <oriatta@post.bgu.ac.il>
2026-01-05 23:31:36 +0000

    Servers support connections, still no protocol

--- server/src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
+++ server/src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
@@ -51,8 +51,7 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
                         if (nextMessage != null) {
                             T response = protocol.process(nextMessage);
                             if (response != null) {
-                                writeQueue.add(ByteBuffer.wrap(encdec.encode(response)));
-                                reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+                                send(response);
                             }
                         }
                     }
@@ -118,6 +117,7 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
 
     @Override
     public void send(T msg) {
-        //IMPLEMENT IF NEEDED
+        writeQueue.add(ByteBuffer.wrap(encdec.encode(msg)));
+        reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
     }
 }
--- server/src/main/java/bgu/spl/net/srv/Reactor.java
+++ server/src/main/java/bgu/spl/net/srv/Reactor.java
@@ -18,6 +18,7 @@ public class Reactor<T> implements Server<T> {
     private final Supplier<MessagingProtocol<T>> protocolFactory;
     private final Supplier<MessageEncoderDecoder<T>> readerFactory;
     private final ActorThreadPool pool;
+    private final ConnectionsImpl<T> connections;
     private Selector selector;
 
     private Thread selectorThread;
@@ -33,6 +34,7 @@ public class Reactor<T> implements Server<T> {
         this.port = port;
         this.protocolFactory = protocolFactory;
         this.readerFactory = readerFactory;
+        this.connections = new ConnectionsImpl<>();
     }
 
     @Override
@@ -100,6 +102,7 @@ public class Reactor<T> implements Server<T> {
                 protocolFactory.get(),
                 clientChan,
                 this);
+        this.connections.connect(handler);
         clientChan.register(selector, SelectionKey.OP_READ, handler);
     }
 
